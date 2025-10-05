fc93d0e5bbf824c0542c91cbfdb95ebbbb191030
Anthony Rodriguez <148160626+RodActual@users.noreply.github.com>
2025-10-04 23:05:33 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,16 @@
 #Entry Point
+
+from flask import Flask
+from flask_cors import CORS
+from routes.auth_routes import auth_bp
+from routes.expense_routes import expense_bp
+
+app = Flask(__name__)
+CORS(app)  # Allow React frontend requests
+
+# Register Blueprints
+app.register_blueprint(auth_bp, url_prefix="/auth")
+app.register_blueprint(expense_bp, url_prefix="/expenses")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
