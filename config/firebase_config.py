bda79305f884d708e62b2aac688d4e645b9d4f3e
Anthony Rodriguez <148160626+RodActual@users.noreply.github.com>
2025-10-04 22:56:15 -0400

    Create firebase_config.py

---
+++ config/firebase_config.py
@@ -0,0 +1,12 @@
+import firebase_admin
+from firebase_admin import credentials, auth, firestore
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate(os.getenv("FIREBASE_CREDENTIALS"))
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
