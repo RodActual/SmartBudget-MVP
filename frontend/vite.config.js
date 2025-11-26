8587b46d390c857f354b69b1f5b287543134d275
Anthony Rodriguez <anthony15s.email@gmail.com>
2025-11-26 16:47:19 -0500

    Updated .json files for prototype building

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,10 +7,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  base: '/SmartBudget-MVP/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 });
